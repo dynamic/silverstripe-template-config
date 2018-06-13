820a46edf2da6cd45fba5fa391463c35064f9000
Jason Irish <jsirish@users.noreply.github.com>
2018-06-13 15:18:17 -0500

    feature - additional logos and icons (#4)
    * feature - additional logos and icons
    
    closes dynamic/core-tools#134
    
    * remove FooterLogoSecondary, publish images onAfterWrite
    
    * phpcs

--- src/ORM/BrandingDataExtension.php
+++ src/ORM/BrandingDataExtension.php
@@ -2,9 +2,11 @@
 
 namespace Dynamic\TemplateConfig\ORM;
 
+use SilverStripe\Assets\File;
 use SilverStripe\Forms\HeaderField;
 use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\ToggleCompositeField;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\AssetAdmin\Forms\UploadField;
@@ -33,6 +35,15 @@ class BrandingDataExtension extends DataExtension
      */
     private static $has_one = array(
         'Logo' => Image::class,
+        'LogoRetina' => Image::class,
+        'FooterLogo' => Image::class,
+        'FooterLogoRetina' => Image::class,
+        'FavIcon' => File::class,
+        'AppleTouchIcon180' => File::class,
+        'AppleTouchIcon152' => File::class,
+        'AppleTouchIcon114' => File::class,
+        'AppleTouchIcon72' => File::class,
+        'AppleTouchIcon57' => File::class
     );
 
     /**
@@ -47,6 +58,10 @@ class BrandingDataExtension extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
+        $logoTypes = array('jpg', 'jpeg', 'png', 'gif', 'svg');
+        $iconTypes = array('ico');
+        $appleTouchTypes = array('png');
+
         $ImageField = UploadField::create('Logo');
         $ImageField->getValidator()->allowedExtensions = array(
             'jpg',
@@ -66,6 +81,16 @@ class BrandingDataExtension extends DataExtension
             'TitleLogo',
             'Title',
             'Tagline',
+            'Logo',
+            'LogoRetina',
+            'FooterLogo',
+            'FooterLogoRetina',
+            'FavIcon',
+            'AppleTouchIcon180',
+            'AppleTouchIcon152',
+            'AppleTouchIcon114',
+            'AppleTouchIcon72',
+            'AppleTouchIcon57',
         ]);
 
         $fields->addFieldsToTab('Root.Main', array(
@@ -74,13 +99,107 @@ class BrandingDataExtension extends DataExtension
             $titlelogo = OptionsetField::create('TitleLogo', 'Branding', $logoOptions),
             $title = TextField::create("Title", _t(SiteConfig::class . '.SITETITLE', "Site title")),
             $tagline = TextField::create("Tagline", _t(SiteConfig::class . '.SITETAGLINE', "Site Tagline/Slogan")),
-            $ImageField,
+            // normal logos
+            $logo = UploadField::create('Logo', 'Logo'),
+            $retinaLogo = UploadField::create('LogoRetina', 'Retina Logo'),
+            // footer logos
+            ToggleCompositeField::create('FooterLogos', 'Footer', [
+                $footerLogo = UploadField::create('FooterLogo', 'Footer Logo'),
+                $footerLogoRetina = UploadField::create('FooterLogoRetina', 'Retina Footer Logo'),
+            ]),
+            // icons
+            ToggleCompositeField::create('Icons', 'Icons', [
+                $favIcon = UploadField::create(
+                    'FavIcon',
+                    'Favicon, in .ico format, dimensions of 16x16, 32x32, or 48x48'
+                ),
+                $appleTouchIcon180 = UploadField::create(
+                    'AppleTouchIcon180',
+                    'Apple Touch Web Clip and Windows 8 Tile Icon (dimensions of 180x180, PNG format)'
+                ),
+                $appleTouchIcon152 = UploadField::create(
+                    'AppleTouchIcon152',
+                    'Apple Touch Web Clip and Windows 8 Tile Icon (dimensions of 152x152, PNG format)'
+                ),
+                $appleTouchIcon114 = UploadField::create(
+                    'AppleTouchIcon114',
+                    'Apple Touch Web Clip and Windows 8 Tile Icon (dimensions of 114x114, PNG format)'
+                ),
+                $appleTouchIcon72 = UploadField::create(
+                    'AppleTouchIcon72',
+                    'Apple Touch Web Clip and Windows 8 Tile Icon (dimensions of 72x72, PNG format)'
+                ),
+                $appleTouchIcon57 = UploadField::create(
+                    'AppleTouchIcon57',
+                    'Apple Touch Web Clip and Windows 8 Tile Icon (dimensions of 57x57, PNG format)'
+                ),
+            ]),
         ));
 
         $title->hideUnless($titlelogo->getName())->isEqualTo('Title');
         $tagline->hideUnless($titlelogo->getName())->isEqualTo('Title');
-        
-        $ImageField->hideUnless($titlelogo->getName())->isEqualTo('Logo');
+
+        $logo->hideUnless($titlelogo->getName())->isEqualTo('Logo');
+        $retinaLogo->hideUnless($titlelogo->getName())->isEqualTo('Logo');
+
+        $logo->getValidator()->setAllowedExtensions($logoTypes);
+        $retinaLogo->getValidator()->setAllowedExtensions($logoTypes);
+        $footerLogo->getValidator()->setAllowedExtensions($logoTypes);
+        $footerLogoRetina->getValidator()->setAllowedExtensions($logoTypes);
+        $favIcon->getValidator()->setAllowedExtensions($iconTypes);
+        $appleTouchIcon180->getValidator()->setAllowedExtensions($appleTouchTypes);
+        $appleTouchIcon152->getValidator()->setAllowedExtensions($appleTouchTypes);
+        $appleTouchIcon114->getValidator()->setAllowedExtensions($appleTouchTypes);
+        $appleTouchIcon72->getValidator()->setAllowedExtensions($appleTouchTypes);
+        $appleTouchIcon57->getValidator()->setAllowedExtensions($appleTouchTypes);
+    }
+
+    /**
+     *
+     */
+    public function onAfterWrite()
+    {
+        parent::onAfterWrite();
+
+        if ($this->owner->Logo()->exists()) {
+            $this->owner->Logo()->publishRecursive();
+        }
+
+        if ($this->owner->LogoRetina()->exists()) {
+            $this->owner->LogoRetina()->publishRecursive();
+        }
+
+        if ($this->owner->FooterLogo()->exists()) {
+            $this->owner->FooterLogo()->publishRecursive();
+        }
+
+        if ($this->owner->FooterLogoRetina()->exists()) {
+            $this->owner->FooterLogoRetina()->publishRecursive();
+        }
+
+        if ($this->owner->FavIcon()->exists()) {
+            $this->owner->FavIcon()->publishRecursive();
+        }
+
+        if ($this->owner->AppleTouchIcon180()->exists()) {
+            $this->owner->AppleTouchIcon180()->publishRecursive();
+        }
+
+        if ($this->owner->AppleTouchIcon152()->exists()) {
+            $this->owner->AppleTouchIcon152()->publishRecursive();
+        }
+
+        if ($this->owner->AppleTouchIcon114()->exists()) {
+            $this->owner->AppleTouchIcon114()->publishRecursive();
+        }
+
+        if ($this->owner->AppleTouchIcon72()->exists()) {
+            $this->owner->AppleTouchIcon72()->publishRecursive();
+        }
+
+        if ($this->owner->AppleTouchIcon57()->exists()) {
+            $this->owner->AppleTouchIcon57()->publishRecursive();
+        }
     }
 
     /**
